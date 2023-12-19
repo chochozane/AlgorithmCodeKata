c2b182a14d7b888dad8b06ad0a1be54b2249c8f7
ZANE <130233619+chochozane@users.noreply.github.com>
2023-12-19 09:42:32 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.20 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,23 @@
+public class Solution 
+{
+    public string solution(int n) 
+    {
+        string answer = "";
+        
+        string su = "수";
+        string bak = "박";
+
+        for (int i = 0; i < n ; i++)
+        {
+            if (i == 0 || i % 2 == 0) // i 가 0 또는 짝수라면
+            {
+                answer += su;
+            }
+            else // i 가 홀수라면
+            {
+                answer += bak;
+            }
+        }
+        return answer;
+    }
+}
