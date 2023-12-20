6613f21a721a5506a596c1803bb4c9d95af7f92c
ZANE <130233619+chochozane@users.noreply.github.com>
2023-12-20 09:32:03 +0900

    [level 1] Title: 내적, Time: 0.18 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.cs
+++ 프로그래머스/1/70128. 내적/내적.cs
@@ -1,25 +1,18 @@
 using System;
-using System.Collections.Generic;
 
 public class Solution 
 {
     public int solution(int[] a, int[] b) 
     {
         int answer = 1234567890;
-        List<int> intList = new List<int>();
-        
-        for (int i = 0; i < a.Length; i++)
-        {
-            intList.Add(a[i]*b[i]);
-        }
-        
         int sum = 0;
-        foreach (int num in intList)
+        
+        for (int i = 0; i < a.Length; i++) 
         {
-            sum += num;
+            sum += a[i] * b[i];
         }
         
-        answer = sum;       
+        answer = sum;
         return answer;
     }
 }
